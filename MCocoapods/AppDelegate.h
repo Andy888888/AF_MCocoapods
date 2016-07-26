2a77aa529296c2c2d3fe319f6eb71a07f32a9aca
燕文强 <yanwenqiang1991@foxmail.com>
2016-07-26 11:02:31 +0800

    Initial Commit

---
+++ MCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCocoapods
+//
+//  Created by 燕文强 on 16/7/26.
+//  Copyright © 2016年 燕文强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCocoapods
+//
+//  Created by 燕文强 on 16/7/26.
+//  Copyright © 2016年 燕文强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
